ab08c3079366f41cc3341f00fd9f570e101fa774
Bruno Berastain <bberastain@gmail.com>
2018-04-10 00:34:22 -0700

    database init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) #creates appliation object as instance of class Flask
 # set to name of module in which it is used
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # not the previously defined variable
+from app import routes, models # not the previously defined variable
 # this is the app package defined by the app directory and __init__.py script
