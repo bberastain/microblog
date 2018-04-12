3074a6d3192bd8e9545c20aa0addee0c65f8dcf6
Bruno Berastain <bberastain@gmail.com>
2018-04-12 16:54:02 -0700

    typos

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Validationerror, Email, EqualTo, DataRequired
+from wtforms.validators import ValidationError, Email, EqualTo, DataRequired
 from app.models import User
 
 class LoginForm(FlaskForm):
