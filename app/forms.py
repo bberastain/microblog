afd1af848115c49bdbae726f4d3e544de97970c2
Bruno Berastain <bberastain@gmail.com>
2018-04-25 11:01:08 -0700

    fixed index methods

--- app/forms.py
+++ app/forms.py
@@ -43,5 +43,5 @@ class EditProfileForm(FlaskForm):
 
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
-    DataRequired(), Length(min=1, max=140)])
+        DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
