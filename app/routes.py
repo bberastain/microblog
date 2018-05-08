b07b1d43e8f5df1dfa0595dd325aba62d60b4508
Vanessa Roca Berastain <vanyroca@Vanessa_iMac.local>
2018-05-07 18:36:29 -0700

    typos

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
